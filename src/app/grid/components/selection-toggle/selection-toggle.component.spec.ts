f29b5f21616c6a6c2918c8d1618de708c1738e05
Stanislau Karmanau <Stanislau_Karmanau@epam.com>
2020-05-02 02:39:36 +0300

    add tests for selection-toggle component

---
+++ src/app/grid/components/selection-toggle/selection-toggle.component.spec.ts
@@ -0,0 +1,42 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { IStatusPanelParams } from 'ag-grid-community';
+
+import { SelectionToggleComponent } from './selection-toggle.component';
+
+describe('SelectionToggleComponent', () => {
+    let selectionToggleComponent: SelectionToggleComponent;
+    let fixture: ComponentFixture<SelectionToggleComponent>;
+
+    let columnApiStub: { setColumnVisible: jasmine.Spy };
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [SelectionToggleComponent],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(SelectionToggleComponent);
+        selectionToggleComponent = fixture.componentInstance;
+
+        columnApiStub = jasmine.createSpyObj('node', ['setColumnVisible']);
+
+        fixture.detectChanges();
+    }));
+
+    it('should be created', () => {
+        expect(selectionToggleComponent).toBeDefined();
+    });
+
+    it('should toggle visibility state while clicking', () => {
+        selectionToggleComponent.agInit(({ columnApi: columnApiStub } as any) as IStatusPanelParams);
+        const button: HTMLButtonElement = fixture.debugElement.query(By.css('button')).nativeElement;
+
+        button.click();
+        fixture.detectChanges();
+        expect(columnApiStub.setColumnVisible).toHaveBeenCalledWith('checkboxColumn', true);
+
+        button.click();
+        fixture.detectChanges();
+        expect(columnApiStub.setColumnVisible).toHaveBeenCalledWith('checkboxColumn', false);
+    });
+});
